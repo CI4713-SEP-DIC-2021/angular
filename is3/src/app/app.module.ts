e27cc0cfe95ef48e119c052db9e6d5ca1e2ea9ba
danielazorrilla <danielazorrilla.dezg@gmail.com>
2021-11-04 22:13:07 -0400

    Projects' component

--- is3/src/app/app.module.ts
+++ is3/src/app/app.module.ts
@@ -8,13 +8,30 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
-import { LoginComponent } from './login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+
+// Components
+import { LoginComponent } from './login/login.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AddProjectComponent } from './projects/dialogs/add-project/add-project.component';
+import { DeleteProjectComponent } from './projects/dialogs/delete-project/delete-project.component';
+import { EditProjectComponent } from './projects/dialogs/edit-project/edit-project.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ProjectsComponent,
+    AddProjectComponent,
+    DeleteProjectComponent,
+    EditProjectComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +43,14 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     MatIconModule,
     MatCardModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatDividerModule,
+    MatDialogModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
